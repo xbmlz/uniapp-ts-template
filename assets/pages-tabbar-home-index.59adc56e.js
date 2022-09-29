3d63fd91193d795e6374fb75f29e5497ce3f9d9b
xbmlz <xbmlz@users.noreply.github.com>
2022-09-29 07:14:21 +0000

    deploy: 80ee026ff4d87b5f571a8251e7a8a3d00bdc684b

---
+++ assets/pages-tabbar-home-index.59adc56e.js
@@ -0,0 +1,1 @@
+import{o as a,c as s,w as e,i as r,a as t,b as n,d as o}from"./index.03444d77.js";import{_ as f}from"./plugin-vue_export-helper.f63bf49c.js";const l=f({},[["render",function(f,l){const u=o,c=r;return a(),s(c,{class:"container"},{default:e((()=>[t(u,null,{default:e((()=>[n("首页")])),_:1})])),_:1})}]]);export{l as default};
