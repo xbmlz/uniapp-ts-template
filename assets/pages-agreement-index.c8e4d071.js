6308936b85e99826aa44be41e819c77ce6ee64db
xbmlz <xbmlz@users.noreply.github.com>
2022-09-30 07:24:30 +0000

    deploy: e2deb6ddf74a6b95aa3832a0fef2fce186a0ff6b

---
+++ assets/pages-agreement-index.c8e4d071.js
@@ -0,0 +1,1 @@
+import{o as a,c as e,w as s,i as r,a as t,b as n,d as o}from"./index.562bc2ea.js";import{_ as l}from"./plugin-vue_export-helper.f63bf49c.js";const u=l({},[["render",function(l,u){const f=o,c=r;return a(),e(c,null,{default:s((()=>[t(f,null,{default:s((()=>[n("服务须知")])),_:1})])),_:1})}]]);export{u as default};
