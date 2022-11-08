160b6ea36fa8bf36ab59695217d4871226e69452
xbmlz <xbmlz@users.noreply.github.com>
2022-11-08 07:04:46 +0000

    deploy: d49a29c9f172985934484abe375702ca7b6068d7

---
+++ assets/pages-agreement-index.69dccddf.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,w as s,i as r,a as t,b as n,d as o}from"./index.2e0be60e.js";import{_ as l}from"./plugin-vue_export-helper.f63bf49c.js";const u=l({},[["render",function(l,u){const f=o,d=r;return e(),a(d,null,{default:s((()=>[t(f,null,{default:s((()=>[n("服务须知")])),_:1})])),_:1})}]]);export{u as default};
---
+++ assets/pages-tabbar-home-index.3de53b4a.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,w as s,i as r,a as t,b as n,d as o}from"./index.2e0be60e.js";import{_ as f}from"./plugin-vue_export-helper.f63bf49c.js";const l=f({},[["render",function(f,l){const u=o,c=r;return e(),a(c,{class:"container"},{default:s((()=>[t(u,null,{default:s((()=>[n("首页")])),_:1})])),_:1})}]]);export{l as default};
